8bb07fa8fb30529dd7e7901eda3803310c7bc549
Adrian Breazu <breazuadrian@gmail.com>
2016-11-05 21:03:27 +0200

    fix bugs

--- src/servo_test.py
+++ src/servo_test.py
@@ -3,11 +3,11 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode (GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
-pwm = GPIO.PWM(18, 100)
+pwm = GPIO.PWM(4, 100)
 pwm.start(5)
 
 class Test:
-    def Update_angle (self, agle):
+    def Update_angle (self, angle):
         new_angle = float(angle) / 10.0 + 2.5
         pwm.ChangeDutyCycle(new_angle)
 
