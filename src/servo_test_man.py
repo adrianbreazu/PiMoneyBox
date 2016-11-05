1a1cda4c819b2b0033ddaa649034d46cb6b6fd36
Adrian Breazu <breazuadrian@gmail.com>
2016-11-05 23:23:19 +0200

    ...

--- src/servo_test_man.py
+++ src/servo_test_man.py
@@ -1,22 +1,26 @@
+# Servo Control
 import time
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT)
-pwm = GPIO.PWM(4, 100)
-pwm.start(2.5)
-
-def change(k):
-    pwm.ChangeDutyCycle(k)
-    time.sleep(1)
-
-try:
-    while True:
-        k = float(input("enter angle: "))
-        k = (1+(float(k)/180))/20
-        change(k)
-
-except KeyboardInterrupt:
-    pwm.stop()
-    GPIO.cleanup()
-    exit
+import wiringpi
+ 
+# use 'GPIO naming'
+wiringpi.wiringPiSetupGpio()
+ 
+# set #18 to be a PWM output
+wiringpi.pinMode(4, wiringpi.GPIO.PWM_OUTPUT)
+ 
+# set the PWM mode to milliseconds stype
+wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+ 
+# divide down clock
+wiringpi.pwmSetClock(192)
+wiringpi.pwmSetRange(2000)
+ 
+delay_period = 0.01
+ 
+while True:
+        for pulse in range(50, 250, 1):
+                wiringpi.pwmWrite(18, pulse)
+                time.sleep(delay_period)
+        for pulse in range(250, 50, -1):
+                wiringpi.pwmWrite(18, pulse)
+                time.sleep(delay_period)
