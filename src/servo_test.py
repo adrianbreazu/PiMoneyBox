415b2e191284787e270adf9457498ab0a32cc0e4
Adrian Breazu <breazuadrian@gmail.com>
2016-11-05 20:30:28 +0200

    virtualenv and servo_test.py

---
+++ src/servo_test.py
@@ -0,0 +1,17 @@
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode (GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+pwm = GPIO.PWM(18, 100)
+pwm.start(5)
+
+class Test:
+    def Update_angle (self, agle):
+        new_angle = float(angle) / 10.0 + 2.5
+        pwm.ChangeDutyCycle(new_angle)
+
+
+if __name__ == "__main__":
+    test = Test()
+    test.Update_angle(150)
